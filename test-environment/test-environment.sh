67ad41a0e7fbf17098ae6bf70313c68fb4a3a023
dypher <i_are_kieran@hotmail.co.uk>
2018-10-21 20:10:59 +0100

    initial test-environment
    only fedora, cronjob doesn't run

---
+++ test-environment/test-environment.sh
@@ -0,0 +1,25 @@
+APP='fedora'
+
+PROJECT_DIR=`git rev-parse --show-toplevel`
+APP_DIR="$PROJECT_DIR/test-environment"
+
+HOST_USER_ID=`id -u`
+HOST_USER_GID=`id -g`
+
+docker kill ansible-$APP
+
+docker build \
+  --build-arg "HOST_USER_ID=$HOST_USER_ID" \
+  --build-arg "HOST_USER_GID=$HOST_USER_GID" \
+  -t docker-apps:ansible-$APP \
+  -f "$APP_DIR/Dockerfile.$APP" $APP_DIR
+
+docker wait ansible-$APP
+
+docker run --rm -d \
+  -v $PROJECT_DIR:/home/kieran/projects/ansible-projects \
+  --hostname=ansible-$APP \
+  --name=ansible-$APP \
+  docker-apps:ansible-$APP
+
+docker exec -it ansible-$APP ansible-playbook -K -v --become workstation.yml
