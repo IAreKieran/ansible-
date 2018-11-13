30c8741480f80bc92c0256ea9e931971a6772ea6
dypher <i_are_kieran@hotmail.co.uk>
2018-11-13 23:04:10 +0000

    added gvm, tidied test-environment script

--- test-environment/test-environment.sh
+++ test-environment/test-environment.sh
@@ -1,23 +1,27 @@
-APP=$1
+OS=$1
+
+if [ "$OS" == "" ]; then
+  OS='fedora'
+fi
 
 PROJECT_DIR=`git rev-parse --show-toplevel`
-APP_DIR="$PROJECT_DIR/test-environment"
+TEST_DIR="$PROJECT_DIR/test-environment"
 
 HOST_USER_ID=`id -u`
 HOST_USER_GID=`id -g`
 
-docker stop ansible-$APP
+docker stop ansible-$OS
 
 docker build \
   --build-arg "HOST_USER_ID=$HOST_USER_ID" \
   --build-arg "HOST_USER_GID=$HOST_USER_GID" \
-  -t docker-apps:ansible-$APP \
-  -f "$APP_DIR/Dockerfile.$APP" $APP_DIR
+  -t docker-apps:ansible-$OS \
+  -f "$TEST_DIR/Dockerfile.$OS" $TEST_DIR
 
 docker run --rm -d \
   -v $PROJECT_DIR:/home/kieran/projects/ansible-projects \
-  --hostname=ansible-$APP \
-  --name=ansible-$APP \
-  docker-apps:ansible-$APP
+  --hostname=ansible-$OS \
+  --name=ansible-$OS \
+  docker-apps:ansible-$OS
 
-docker exec -it ansible-$APP ansible-playbook -K -v --become workstation.yml
+docker exec -it ansible-$OS ansible-playbook -K -v --become workstation.yml
